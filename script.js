d997c69e854a7f547fb837037ec33c27e582c5fe
devadityadev <raghavendra91440@gmail.com>
2024-12-04 23:21:34 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const ham = document.getElementById('ham')
+
+function hamburg(){
+    ham.style.transform = "translateY(0px)";
+}
+function  cancel(){
+    ham.style.transform = "translateY(-400px)";
+}
